93bfca101aea0e9b01e703510ad8dd08d9c850d9
H. Abbasi <72363836+abbasi08@users.noreply.github.com>
2022-03-31 20:37:36 -0400

    added models

--- db/index.js
+++ db/index.js
@@ -4,6 +4,22 @@ const sequelize = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost/the_acme_tasks_db"
 );
 
+const Task = sequelize.define("task", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+const User = sequelize.define("user", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
 module.exports = {
   sequelize,
+  Task,
+  User,
 };
--- server.js
+++ server.js
@@ -6,12 +6,41 @@ const app = express();
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
+app.get("/api/tasks", async (req, res, next) => {
+  try {
+    res.send(await Task.findAll());
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await User.findAll());
+  } catch (error) {
+    next(error);
+  }
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 const init = async () => {
-  console.log("calling init");
+  console.log("Initializing database..");
   await sequelize.sync({ force: true });
+
+  await Promise.all([
+    User.create({ firstName: "Hannah" }),
+    User.create({ firstName: "Lauren" }),
+    User.create({ firstName: "Joshua" }),
+  ]);
+
+  await Promise.all([
+    Task.create({ name: "answer questions" }),
+    Task.create({ name: "go shopping" }),
+    Task.create({ name: "email mentors" }),
+    Task.create({ name: "do all the hw" }),
+  ]);
 };
 
 init();
